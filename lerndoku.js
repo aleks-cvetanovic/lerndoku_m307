e6cc96395a2c75d578eb9e831e6662d9d57f37c9
aleks-cvetanovic <aleks.cvetanovic@bbz-sh.ch>
2019-03-25 15:14:14 +0100

    push

---
+++ lerndoku.js
@@ -0,0 +1,22 @@
+
+
+
+
+var Beispiel;
+
+Beispiel = 3;
+
+
+//NUMMERISCHE DATEN
+//---------------------
+0.75
+
+
+//STRINGDATEN
+//---------------------
+"Hi, Ivy!"
+
+
+//BOOLESCHE DATEN
+//---------------------
+true
