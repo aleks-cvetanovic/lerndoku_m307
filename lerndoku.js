5c2c33533b9e7273c8f0b80c5523e38de545ef90
aleks-cvetanovic <aleks.cvetanovic@bbz-sh.ch>
2019-03-26 09:41:34 +0100

    bla

--- lerndoku.js
+++ lerndoku.js
@@ -23,3 +23,11 @@ true
 
 
 
+var text = ["text","text"];
+text (1) = Banane;
+console.log();
+
+
+var Beispiel_2 = ["Banane","Apfel","Gurke"]; <br/>
+Beispiel_2 (2) = "Mandarine";
+console.log(Beispiel_2);
